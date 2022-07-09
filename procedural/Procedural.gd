42a9b7385be3cdad0e0e412dde851894eda45a6b
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2022-07-10 00:45:12 +0200

    Add procedural geometry generation basic sample

---
+++ procedural/Procedural.gd
@@ -0,0 +1,37 @@
+extends Spatial
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var st = SurfaceTool.new()
+
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+
+	# Prepare attributes for add_vertex.
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(0, 0))
+	# Call last for each vertex, adds the above attributes.
+	st.add_vertex(Vector3(-1, -1, 0))
+
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(0, 1))
+	st.add_vertex(Vector3(-1, 1, 0))
+
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(1, 1))
+	st.add_vertex(Vector3(1, 1, 0))
+
+	# Commit to a mesh.
+	var mesh = st.commit()
+	
+	var myMesh = MeshInstance.new()
+	myMesh.mesh = mesh
+	add_child(myMesh)
+	
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
