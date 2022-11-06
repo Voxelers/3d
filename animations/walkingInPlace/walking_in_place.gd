41960ff44be23346f654f18ef10848c4b54626fc
Alvaro del Castillo San Felix <adelcastillo@monom.ai>
2022-11-06 12:29:58 +0100

    Improve walking animation logic

--- animations/walkingInPlace/walking_in_place.gd
+++ animations/walkingInPlace/walking_in_place.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 const step_distance = 0.015
 const step_distance_up = 0.1
+var backwards = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +12,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var pos = $Armature.get_global_position()
-	var new_pos = Vector3(pos.x, pos.y, pos.z + step_distance)
-	# $Armature.set_position(new_pos)
+	if $AnimationPlayer.is_playing():
+		var pos = $Armature.get_global_position()
+		var new_pos
+		if ! backwards:
+			new_pos = Vector3(pos.x, pos.y, pos.z + step_distance)
+		else:
+			new_pos = Vector3(pos.x, pos.y, pos.z - step_distance)
+		
+		$Armature.set_position(new_pos)
 
 func _on_animation_player_animation_finished(anim_name):
 	# $AnimationPlayer.play("Armature|mixamocom|Layer0")
@@ -22,10 +29,13 @@ func _on_animation_player_animation_finished(anim_name):
 func _input(event):
 	var pos = $Armature.get_global_position()
 	if Input.is_action_pressed('ui_up'):
-		$AnimationPlayer.play('Armature|mixamocom|Layer0', -1, 1)
-		# await get_tree().create_timer(0.5).timeout
-		$Armature.set_position(Vector3(pos.x, pos.y, pos.z + step_distance_up))
-		# $AnimationPlayer.stop(false)
+		if ! $AnimationPlayer.is_playing():
+			backwards = false
+			$AnimationPlayer.play('Armature|mixamocom|Layer0', -1, 2)
+			# await get_tree().create_timer(0.5).timeout
+			# $AnimationPlayer.stop(false)
 	elif Input.is_action_pressed('ui_down'):
-		$AnimationPlayer.play_backwards('Armature|mixamocom|Layer0')
-		$Armature.set_position(Vector3(pos.x, pos.y, pos.z - step_distance_up))
+		if ! $AnimationPlayer.is_playing():
+			backwards = true
+			$AnimationPlayer.play('Armature|mixamocom|Layer0', -1, -2, true)
+
