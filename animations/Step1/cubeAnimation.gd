79184d5f5fb61343fe42bbd1c618290cafc232f9
Alvaro del Castillo San Felix <adelcastillo@monom.ai>
2023-05-07 21:57:10 +0200

    Add metaballs anim

--- animations/Step1/cubeAnimation.gd
+++ animations/Step1/cubeAnimation.gd
@@ -4,7 +4,10 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
     var player = $cubeArmature/AnimationPlayer
+    var anim = player.get_animation("ArmatureAction")
+    anim.set_loop_mode(1)
     player.play("ArmatureAction") # Replace with function body.
+    $RigidBody3D/metaballsBasicAnim/AnimationPlayer.play("ArmatureAction")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
